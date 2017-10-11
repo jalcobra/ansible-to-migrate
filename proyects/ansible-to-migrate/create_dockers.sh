34a0c245b603a118c307a35554711a12f4a76a88
jalcobra <31253418+jalcobra@users.noreply.github.com>
2017-10-11 13:50:15 -0500

    Update create_dockers.sh

--- proyects/ansible-to-migrate/create_dockers.sh
+++ proyects/ansible-to-migrate/create_dockers.sh
@@ -7,11 +7,7 @@ echo "----------------------"
 echo "Lanzando servidor MySQL"
 docker run -d -P --name server02 -p 2222:22 -p 3306:3306 $1
 echo "Servidor desplegado."
-echo
-echo "----------------------"
-echo "Lanzando servidor noSQL Redis"
-docker run -d -P --name server03 -p 2223:22 -p 6379:6379 $1
-echo "Servidor desplegado."
+
 echo
 echo "----------------------"
 echo "     Confirmando      "
